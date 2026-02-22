0c202a34e09c7423889df003cbfe785dd13a6b7d
art-web <58181@lomsak.ac.th>
2026-02-22 21:19:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ cursor.style.left = e.clientX+"px"
 cursor.style.top = e.clientY+"px"
 
 })
+
+/* parallax iPhone style */
+
+document.addEventListener("mousemove",(e)=>{
+
+let x = (window.innerWidth/2 - e.clientX)/40
+let y = (window.innerHeight/2 - e.clientY)/40
+
+document.querySelector(".hero").style.transform =
+`translate(${x}px,${y}px)`
+
+})
